46150de84333e05636c87811ea157ce1e4f931d4
Kian <kianzyk@KiandeMBP.gateway>
2017-01-13 15:38:23 +1000

    font & bg color

--- source/assets/javascripts/all.js
+++ source/assets/javascripts/all.js
@@ -1,13 +1,14 @@
-//= require instafeed.min
+//= require jquery-3.1.1.min.js
+
 
 // This is where it all goes :)
-var loadButton = document.getElementById('load-more');
-var feed = new Instafeed({
-  get: 'user',
-  tagName: 'VSCO',
-  userId: '411688063',
-  accessToken: '411688063.1677ed0.b3558c060d6a494bb28ff21b4dc7cbd5',
-  resolution: 'low_resolution'
+// var loadButton = document.getElementById('load-more');
+// var feed = new Instafeed({
+//   get: 'user',
+//   tagName: 'VSCO',
+//   userId: '411688063',
+//   accessToken: '411688063.1677ed0.b3558c060d6a494bb28ff21b4dc7cbd5',
+//   resolution: 'low_resolution'
 
-});
-feed.run();
+// });
+// feed.run();
