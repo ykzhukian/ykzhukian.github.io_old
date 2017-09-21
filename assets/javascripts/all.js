8e222f142913f5781fd530ae9fde98d0b3173718
Kian <kianzyk@gmail.com>
2017-09-21 13:08:04 +0800

    Automated commit at 2017-09-21 05:08:04 UTC by middleman-deploy 2.0.0-alpha

--- assets/javascripts/all.js
+++ assets/javascripts/all.js
@@ -35,31 +35,69 @@ $("#nav-link-intro").on("mouseover", function(){
 })
 
 $("#nav-link-intro").on("mouseleave", function(){
-  $(this).find("img").attr('src', '../assets/images/soda_grey.svg');
+  if ($(this).hasClass("active")) {
+    $(this).find("img").attr('src', '../assets/images/soda.svg');
+  } else {
+    $(this).find("img").attr('src', '../assets/images/soda_grey.svg');
+  }
 })
+
+if ($("#nav-link-intro").hasClass("active")) {
+  $("#nav-link-intro").find("img").attr('src', '../assets/images/soda.svg');
+}
 
 $("#nav-link-code").on("mouseover", function(){
   $(this).find("img").attr('src', '../assets/images/keyboard.svg');
 })
 
 $("#nav-link-code").on("mouseleave", function(){
-  $(this).find("img").attr('src', '../assets/images/keyboard_grey.svg');
+  if ($(this).hasClass("active")) {
+    $(this).find("img").attr('src', '../assets/images/keyboard.svg');
+  } else {
+    $(this).find("img").attr('src', '../assets/images/keyboard_grey.svg');
+  }
 })
+
+if ($("#nav-link-code").hasClass("active")) {
+  $("#nav-link-code").find("img").attr('src', '../assets/images/keyboard.svg');
+}
 
 $("#nav-link-pin").on("mouseover", function(){
   $(this).find("img").attr('src', '../assets/images/pin.svg');
 })
 
 $("#nav-link-pin").on("mouseleave", function(){
-  $(this).find("img").attr('src', '../assets/images/pin_grey.svg');
+  if ($(this).hasClass("active")) {
+    $(this).find("img").attr('src', '../assets/images/pin.svg');
+  } else {
+    $(this).find("img").attr('src', '../assets/images/pin_grey.svg');
+  }
 })
+
+if ($("#nav-link-pin").hasClass("active")) {
+  $("#nav-link-pin").find("img").attr('src', '../assets/images/pin.svg');
+}
 
 $("#nav-link-mail").on("mouseover", function(){
   $(this).find("img").attr('src', '../assets/images/mail.svg');
 })
 
 $("#nav-link-mail").on("mouseleave", function(){
-  $(this).find("img").attr('src', '../assets/images/mail_grey.svg');
+  if ($(this).hasClass("active")) {
+    $(this).find("img").attr('src', '../assets/images/mail.svg');
+  } else {
+    $(this).find("img").attr('src', '../assets/images/mail_grey.svg');
+  }
 })
 
-;
+if ($("#nav-link-mail").hasClass("active")) {
+  $("#nav-link-mail").find("img").attr('src', '../assets/images/mail.svg');
+}
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 100) {
+    $("#sider-container").addClass("active")
+  } else {
+    $("#sider-container").removeClass("active")
+  }
+});
